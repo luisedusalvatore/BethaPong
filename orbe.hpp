1e75531f73598412c27be5f8d3355d967d1386c4
Luis Salvatore <luiseduardosalvatore@gmail.com>
2026-09-20 16:05:05 -0300

    Criação da primeira versão da bibliotera Orbe.cpp que tem a função de controlar e manipular a bola

---
+++ orbe.hpp
@@ -0,0 +1,90 @@
+#ifndef ORBE_HPP
+#define ORBE_HPP
+#include <ctime>
+#include <cstdlib>
+#include <cmath>
+#include <raylib.h>
+#include "coisas.hpp"
+
+class Orbe {
+    private:
+        
+        int altura = 12;
+        int largura = 12;
+
+        double x = (WIDTH - largura)/2;
+        double y = (HEIGHT - altura)/2;
+
+        double v_max = 5;
+        double vx = 3;
+        double vy = 4;
+
+        
+
+    public:
+        int verifica_posi(){
+            
+            if(x + vx <= 0) return 1;
+            if(x + largura + vx >= WIDTH) return 2;
+            if(y + vy <= 0) return 3;
+            if(y + altura + vy >= HEIGHT) return 3;
+            return 0;
+            
+        }
+
+        void ajusta_v(){
+            double v = sqrt(pow(vx, 2)+pow(vy, 2));
+            vx = (vx/v)*v_max;
+            vy = (vy/v)*v_max;
+            
+        }
+
+        void atualiza_posi(){
+            x += vx;
+            y += vy;
+        }
+
+        void bate_parede(){
+            if(verifica_posi() == 3)
+            vy *= -1;
+        }
+        void desenha_orbe(){
+            DrawRectangle(x, y, largura, altura, RAYWHITE);
+        }
+        void reseta_posi(){
+            srand(time(0));
+            int gerador = rand()%2;
+            if(verifica_posi() == 1){
+                if(gerador == 0){
+                   x = ((WIDTH - largura)/2);
+                   y = ((HEIGHT - altura)/2) + 40;
+                   vx = -4;
+                   vy = -5;
+                }
+                else{
+                   x = ((WIDTH - largura)/2);
+                   y = ((HEIGHT - altura)/2) - 40;
+                   vx = -4;
+                   vy = 5;
+                }
+            }
+            else if(verifica_posi() == 2){
+                if(gerador == 0){
+                   x = ((WIDTH - largura)/2);
+                   y = ((HEIGHT - altura)/2) + 40;
+                   vx = 4;
+                   vy = -5;
+                }
+                else{
+                   x = ((WIDTH - largura)/2);
+                   y = ((HEIGHT - altura)/2) - 40;
+                   vx = 4;
+                   vy = 5;
+                }
+            }
+            ajusta_v();
+        }
+        
+
+};
+#endif

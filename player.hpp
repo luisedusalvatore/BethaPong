c22c92f93154ac532f34222df2f141160d932f71
Luis Salvatore <luiseduardosalvatore@gmail.com>
2026-09-20 14:48:40 -0300

    Criação da classe Player com a raquete do jogador e as suas movimentações

---
+++ player.hpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <raylib.h>
+using namespace std;
+#define WIDTH 800
+#define HEIGHT 450
+
+class Player{
+    private:
+        int placar;
+        int altura = 80;
+        int largura = 12;
+        int x = 0;
+        int y = (HEIGHT - altura)/2;
+        int vel = 5;
+        int verifica_altura(int sinal){
+            if(sinal > 0){
+                if((y + altura + vel) > HEIGHT) return 0;
+            }
+            else{
+                if((y - vel) < 0) return 0;
+            }
+            return 1;
+        }
+        
+    public:
+        void setX(int posi){
+            x = (posi - largura)/2;
+        }
+        void moveY(int sinal){
+            if(verifica_altura(sinal)) y += sinal * vel;
+        }
+        void desenha_peça(){
+            DrawRectangle(x, y, largura, altura, RAYWHITE);
+        }
+};

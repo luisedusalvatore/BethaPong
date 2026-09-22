2f42d36caadd835a26105285adf4e42d04cab45e
Luis Salvatore <luiseduardosalvatore@gmail.com>
2026-09-22 12:00:13 -0300

    Criação dos textos e botoes da tela de inicio

--- interacoes.hpp
+++ interacoes.hpp
@@ -9,4 +9,33 @@ void detectar_colisoes(Player *player, Orbe *orbe, int sinal){
         orbe->bate_raquete(player->getVel(), sinal);
     }
 }
+void inicio_desenha_texto(){
+    int tam = 60;
+    const char* texto = "Para inciar o jogo pressione:";
+    const char* texto2 = "INICIAR ou aperte a tecla ESPAÇO";
+    int altura = tam;
+    int largura = MeasureText(texto, tam);
+    int x = (WIDTH - largura)/2;
+    int y = ((HEIGHT - altura)/2 - (tam/2)) - 200;
+    DrawText(texto, x, y, tam, RAYWHITE);
+    int largura2 = MeasureText(texto2, tam);
+    int x2 = (WIDTH - largura2)/2;
+    int y2 = ((HEIGHT - altura)/2 + (tam/2)) - 200;
+    DrawText(texto2, x2, y2, tam, RAYWHITE);
+}
+void inicio_desenha_botao(){
+    int tam = 60;
+    int grossura = 30;
+    const char* texto = "INICIAR";
+    int altura = tam;
+    int largura = MeasureText(texto, tam);
+    int x = (WIDTH - largura)/2;
+    int y = (HEIGHT - altura)/2;
+    int altura_botao = altura + grossura;
+    int largura_botao = largura + grossura;
+    int x_botao = (WIDTH - largura_botao)/2;
+    int y_botao = (HEIGHT - altura_botao)/2;
+    DrawRectangle(x_botao, y_botao, largura_botao, altura_botao, RAYWHITE);
+    DrawText(texto, x, y, tam, PRETO_CINZA);
+}
 #endif
